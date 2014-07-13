90dc203e0b6445a66c9d9d7f01bab59cb4a85e3f
0xhex <efekanpulatli@hotmail.com>
2014-07-13 15:29:45 +0300

    Upload

---
+++ 18xx Knight Online Koxp Project/Console1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace ZeusAFK_koxp.NET
+{
+    public partial class Console1 : Form1
+    {
+
+        internal static class NativeMethods
+        {
+            [DllImport("kernel32.dll")]
+            internal static extern Boolean AllocConsole();
+        }
+
+        public void openConsole()
+        {
+            NativeMethods.AllocConsole();
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+
+
+    }
+    }
